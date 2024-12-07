8b307892fb98d7d1d51646516e590c2c379ac051
Dobri <79987000+NehuaV@users.noreply.github.com>
2024-12-07 01:27:16 +0100

    improvements

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import { chromium } from "playwright";
-import { mkdir, stat } from "fs/promises";
+import { firefox, type BrowserContext } from "playwright";
+import { mkdir, stat, readdir } from "fs/promises";
 import { basename } from "path";
 import sharp from "sharp";
+import { PlaywrightBlocker } from "@cliqz/adblocker-playwright";
+// import fetch from "cross-fetch"; // If bun poses problems, uncomment this & install
 
 async function ensureDirExists(dir: string) {
   try {
@@ -42,28 +44,36 @@ type Options = {
 
 async function main(
   targetUrl: string,
+  context: BrowserContext,
   options: Options = {
     scopeSelector: "//html/body",
     imagesDir: "./images",
   }
 ) {
-  await ensureDirExists(options.imagesDir);
+  const targetUrlPaths = new URL(targetUrl).pathname.split("/").slice(2, 5);
+  const len = targetUrlPaths.length;
+  targetUrlPaths[len - 2] = targetUrlPaths[len - 2] + targetUrlPaths[len - 1];
+  targetUrlPaths.pop();
+  const fullPath = targetUrlPaths.join("/");
 
-  const browser = await chromium.launch({
-    headless: true,
-    args: ["--window-size=3840,2160"],
-  });
+  const directory = `${options.imagesDir}/${fullPath}`;
+  console.log("Upserting directory:", directory);
+  await ensureDirExists(directory);
 
-  const context = await browser.newContext({
-    viewport: { width: 3840, height: 2160 },
-  });
+  // Setup the ad blocker before creating pages
+  const blocker = await PlaywrightBlocker.fromLists(fetch, [
+    "https://easylist.to/easylist/easylist.txt",
+    // You can add more filter lists if you like
+    // 'https://easylist-downloads.adblockplus.org/uce.txt',
+  ]);
 
   const page = await context.newPage();
+  await blocker.enableBlockingInPage(page);
 
   console.log("Navigating to page...");
   await page.goto(targetUrl);
+  await page.waitForLoadState("networkidle");
 
-  await page.waitForLoadState("domcontentloaded");
   console.log("Extracting image URLs...");
 
   const imageUrls = await page
@@ -78,22 +88,42 @@ async function main(
 
   console.log(`Found ${imageUrls.length} images meeting size requirements.`);
 
+  // If images count are the same as the number of images in the directory, we are done
+  if (imageUrls.length === (await readdir(directory)).length) {
+    console.log("All images already downloaded.");
+    await page.close();
+    return;
+  }
+
   for (const imageUrl of imageUrls) {
     if (imageUrl.startsWith("http")) {
-      await downloadImage(imageUrl, options.imagesDir);
+      await downloadImage(imageUrl, directory);
     } else {
       console.warn(`Skipping non-http image URL: ${imageUrl}`);
     }
   }
 
-  await browser.close();
+  await page.close();
   console.log("All done!");
 }
 
-main(
+const chapterarr = [
   "https://asuracomic.net/series/the-regressed-mercenarys-machinations-175de8e7/chapter/1",
-  {
-    scopeSelector: "//html/body/div[3]/div/div/div/div[5]",
+  "https://asuracomic.net/series/the-regressed-mercenarys-machinations-175de8e7/chapter/2",
+];
+
+const browser = await firefox.launch({
+  headless: true,
+});
+const context = await browser.newContext();
+
+for (const chapter of chapterarr) {
+  await main(chapter, context, {
+    scopeSelector: "//html/body/div[3]/div/div/div/div[5]/div",
     imagesDir: "./images",
-  }
-);
+  });
+}
+
+await context.close();
+await browser.close();
+process.exit(0);
